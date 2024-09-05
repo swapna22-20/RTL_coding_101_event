3e587fd1971d890da3bc095957619313d4c3adf1
swapna22-20 <swapnayanamala22@gmail.com>
2024-09-06 00:45:19 +0530

    Day1.v

--- TASK-1/Day1/Day1.v
+++ TASK-1/Day1/Day1.v
@@ -1,1 +1,61 @@
+1.AND GATE
 
+module LOGIC_GATES (
+input A, B,
+output Y0
+);
+assign Y0=(A&B);
+endmodule
+
+2. OR GATE 
+
+module LOGIC_GATES (
+input A, B,
+output Y0
+);
+assign Y0= (A|B);
+endmodule
+
+3. NOT GATE 
+
+module LOGIC_GATES (
+input A, B,
+output Y0, Y1
+);
+assign Y0= ! (A) ;
+assign Y1 =! (B) ;
+endmodule
+
+4.NOR GATE 
+
+module LOGIC_GATES (
+input A, B,
+output Y0
+assign Y0= | (A|B);
+
+5. NAND GATE
+  
+module LOGIC_GATES (
+input A, B,
+output Y0
+);
+assign Y0= ! (A&B) ;
+endmodule
+
+6. XOR GATE 
+  
+module LOGIC_GATES (
+input A, B,
+output Y0
+);
+assign Y0= ( (A) & (!B) | (!A) &(B) );
+endmodule
+
+7. XNOR GATE 
+ 
+module LOGIC_GATES (
+input A, B,
+output Y0
+);
+assign Y0= ((A&B) | (!(A&B)));
+endmodule
