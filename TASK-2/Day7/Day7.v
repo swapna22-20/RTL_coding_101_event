c3891348de5adddb194ccdcad01d430458f2d679
swapna22-20 <swapnayanamala22@gmail.com>
2024-09-06 09:38:05 +0530

    Day7.v

---
+++ TASK-2/Day7/Day7.v
@@ -0,0 +1,19 @@
+module pos_edge_det ( input sig,            // Input signal for which positive edge has to be detected
+                      input clk,            // Input signal for clock
+                      input rstn,
+                      output pe);           // Output signal that gives a pulse when a positive edge occurs
+ 
+    reg   sig_dly;                          // Internal signal to store the delayed version of signal
+ 
+    // This always block ensures that sig_dly is exactly 1 clock behind sig
+  always @ (posedge clk) begin
+    if(!rstn)
+    sig_dly <= 0;
+    else
+    sig_dly <= sig;
+  end
+ 
+    // Combinational logic where sig is AND with delayed, inverted version of sig
+    // Assign statement assigns the evaluated expression in the RHS to the internal net pe
+  assign pe = sig & ~sig_dly;            
+endmodule 
